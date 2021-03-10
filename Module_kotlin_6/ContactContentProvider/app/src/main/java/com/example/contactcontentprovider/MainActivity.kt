1b7740b6a1fcf978f045335c5acf78afe6e0e7b0
Vasilevskiy Pasha <vasilevskij00@inbox.ru>
2021-03-10 19:41:34 +0300

    feat: add a contact update

--- Module_kotlin_6/ContactContentProvider/app/src/main/java/com/example/contactcontentprovider/MainActivity.kt
+++ Module_kotlin_6/ContactContentProvider/app/src/main/java/com/example/contactcontentprovider/MainActivity.kt
@@ -4,8 +4,10 @@ import android.Manifest
 import android.content.Intent
 import androidx.core.app.ActivityCompat;
 import android.content.pm.PackageManager
+import android.net.Uri
 import android.os.Bundle
 import android.provider.ContactsContract
+import android.view.View
 import android.widget.*
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat
@@ -13,10 +15,11 @@ import androidx.core.content.ContextCompat
 class MainActivity : AppCompatActivity() {
 
     private val REQUEST_CODE_READ_CONTACTS = 1
+
     var cols = listOf<String>(
+        ContactsContract.CommonDataKinds.Phone.LOOKUP_KEY,
         ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME,
         ContactsContract.CommonDataKinds.Phone.NUMBER,
-
         ContactsContract.CommonDataKinds.Phone._ID
     ).toTypedArray()
 
@@ -90,5 +93,54 @@ class MainActivity : AppCompatActivity() {
                 return false
             }
         })
+
+        listView.onItemClickListener = object : AdapterView.OnItemClickListener {
+
+            override fun onItemClick(
+                parent: AdapterView<*>?,
+                view: View?,
+                position: Int,
+                id: Long
+            ) {
+
+                var lookupKeyIndex = 0
+
+                var idIndex = 0
+
+                var currentLookupKey: String? = null
+
+                var currentId: Long = 0
+
+                var selectedContactUri: Uri? = null
+
+                rs?.apply {
+                    lookupKeyIndex = getColumnIndex(ContactsContract.Contacts.LOOKUP_KEY)
+
+                    currentLookupKey = getString(lookupKeyIndex)
+
+                    idIndex = getColumnIndex(ContactsContract.Contacts._ID)
+
+                    currentId = getLong(idIndex)
+
+                    selectedContactUri =
+                        ContactsContract.Contacts.getLookupUri(currentId, currentLookupKey)
+                }
+
+                val editIntent = Intent(Intent.ACTION_EDIT).apply {
+                    setDataAndType(selectedContactUri, ContactsContract.Contacts.CONTENT_ITEM_TYPE)
+                    putExtra("finishActivityOnSaveCompleted", true)
+                }
+
+                startActivity(editIntent)
+            }
+        }
+    }
+
+    /*
+    * Refresh our listView after changing the contact
+    * */
+    override fun onResume() {
+        super.onResume()
+        readContact()
     }
 }
